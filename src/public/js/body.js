8f2a1bad09a58e4a301e858d68647e4badcb7012
unknown <shiningdev84@gmail.com>
2021-01-02 20:07:38 +0700

    menu-detail

---
+++ src/public/js/body.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     if(this.scrollY > 20){
+//       $('.container-fluid').addClass('sticky');
+//     }
+//   })
+// })
---
+++ src/public/js/header.js
@@ -0,0 +1,33 @@
+// menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "328px"; 
+    document.getElementById("body").style.opacity = "0.2";
+    document.body.style.overflow = "hidden";
+  }    
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("body").style.opacity = "1";
+    document.body.style.overflow = "auto";
+  }
+  // account-right
+  function openNav_right() {
+    document.getElementById("mySidenav_right").style.width = "328px";
+    document.getElementById("body").style.opacity = "0.2";
+    document.body.style.overflow = "hidden";
+  }
+  
+  function closeNav_right(){
+    document.getElementById("mySidenav_right").style.width = "0";
+    document.getElementById("body").style.opacity = "1";
+    document.body.style.overflow = "auto";
+  }
+
+
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if(this.scrollY > 20){
+        $(".container-fluid").addClass("sticky");
+      }
+    })
+  });
+  
